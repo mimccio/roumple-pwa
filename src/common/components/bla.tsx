9704b016a08499adab27039e79c348ee469ebae1
mimccio <miklmccio@gmail.com>
2023-05-08 05:27:25 +0200

    add tailwind prettier plugin

--- src/common/components/bla.tsx
+++ src/common/components/bla.tsx
@@ -1,3 +1,3 @@
 export function Bla() {
-  return <div className="text-3xl font-bold underline">Bla Component</div>
+  return <div className="text-3xl font-bold text-indigo-500 underline">Bla Component</div>
 }
