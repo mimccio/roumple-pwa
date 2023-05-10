76262f36e902cfaa583505136c72603c1ad9c686
mimccio <miklmccio@gmail.com>
2023-05-10 20:04:53 +0200

    remove webfontDownload

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
-import webfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    webfontDownload(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: { enabled: false },
