0a736fdcc693630349ec4c21e9fd900b0089430a
mimccio <miklmccio@gmail.com>
2023-05-28 21:29:46 +0200

    handle loading

--- src/modules/routine/hooks/use-board-routines.ts
+++ src/modules/routine/hooks/use-board-routines.ts
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useAtom } from 'jotai'
 
 import { getTodayDate } from '&/common/utils'
-import type { Routine, ScheduleType } from '../types'
+import type { ScheduleType } from '../types'
 import { BOARD, ROUTINE } from '../constants'
 import { fetchBoardRoutines } from '../queries'
 import { categoryAtom } from '&/modules/category/atoms'
@@ -18,22 +18,12 @@ export function useBoardRoutines({ type }: Params) {
   const [showDone, setShowDone] = useState(false)
   const [showPeriod, setShowPeriod] = useState(true)
   const [category] = useAtom(categoryAtom)
-
   const { data, isLoading, error } = useQuery([ROUTINE, BOARD, { date, type }], fetchBoardRoutines)
-
-  const [routines, setRoutines] = useState<Routine[] | null | undefined>(
-    data?.filter((routine) => filterRoutines({ routine, category, showDone }))
-  )
-
-  useEffect(() => {
-    setRoutines(data?.filter((routine) => filterRoutines({ routine, category, showDone })))
-  }, [data, category, showDone])
 
   const handleShowDone = () => setShowDone((prevState) => !prevState)
   const handleShowPeriod = () => setShowPeriod((prevState) => !prevState)
-
   const isError = Boolean(error)
-
+  const routines = data?.filter((routine) => filterRoutines({ routine, category, showDone }))
   const isEmpty = !error && !isLoading && !routines?.length
 
   return { routines, isLoading, handleShowDone, showDone, date, isError, isEmpty, showPeriod, handleShowPeriod }
