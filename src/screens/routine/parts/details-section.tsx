7b367042733beafc5b6aaca8c88efa50a72e6987
mimccio <miklmccio@gmail.com>
2023-06-01 20:00:34 +0200

    created at

--- src/screens/routine/parts/details-section.tsx
+++ src/screens/routine/parts/details-section.tsx
@@ -2,6 +2,7 @@ import { StatusSelector } from '&/common/components/inputs/status-selector'
 import { STATUSES } from '&/common/constants/statuses'
 import { cl } from '&/common/utils'
 import { Routine } from '&/modules/routine/types'
+import { CreatedAt } from './created-at'
 import { Priority } from './priority'
 
 interface Props {
@@ -16,13 +17,16 @@ export function DetailsSection({ routine }: Props) {
   return (
     <section
       className={cl(
-        'flex flex-col border-b p-4',
+        'flex flex-col gap-4 border-b p-4',
         isDone ? 'border-green-100 bg-green-50' : 'border-gray-200 bg-gray-100'
       )}
     >
       <div className="flex items-center justify-between">
         <StatusSelector />
         <Priority routine={routine} />
+      </div>
+      <div>
+        <CreatedAt createdAt={routine.created_at} />
       </div>
     </section>
   )
