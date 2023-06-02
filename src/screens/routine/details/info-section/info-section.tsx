9766f943d120c36290293131a8427274c63d2b2b
mimccio <miklmccio@gmail.com>
2023-06-02 03:20:50 +0200

    remove green background for done

--- src/screens/routine/details/info-section/info-section.tsx
+++ src/screens/routine/details/info-section/info-section.tsx
@@ -1,5 +1,3 @@
-import { STATUSES } from '&/common/constants'
-import { cl } from '&/common/utils'
 import { StatusSelector } from '&/common/components/inputs/status-selector'
 import type { Routine } from '&/modules/routine/types'
 import { Priority, RoutineCategory, CreatedAt, RoutineSchedule } from './parts'
@@ -9,15 +7,8 @@ interface Props {
 }
 
 export function InfoSection({ routine }: Props) {
-  const isDone = routine.actions?.[0]?.status === STATUSES.done
-
   return (
-    <section
-      className={cl(
-        'flex flex-col gap-4 border-b p-4',
-        isDone ? 'border-green-100 bg-green-50' : 'border-gray-200 bg-gray-100'
-      )}
-    >
+    <section className="flex flex-col gap-4 border-b bg-gray-100 p-4">
       <div className="flex items-center justify-between">
         <StatusSelector />
         <Priority routine={routine} />
