7e6222b520e646d24395e2ee085b56cc7439c4bd
mimccio <miklmccio@gmail.com>
2023-06-04 14:12:54 +0200

    checklist

--- src/screens/routine/details/main-section/parts/checklist/routine-checklist.tsx
+++ src/screens/routine/details/main-section/parts/checklist/routine-checklist.tsx
@@ -1,4 +1,5 @@
 import { useDeleteChecklistItem } from '&/modules/routine-checklist-item/hooks/use-delete-checklist-item'
+import { SCHEDULE_TYPES } from '&/modules/routine/constants'
 import { useUpsertAction } from '&/modules/routine/hooks'
 import { Routine } from '&/modules/routine/types'
 import { ChecklistItem } from './checklist-item'
@@ -20,10 +21,18 @@ export function RoutineChecklist({ routine, date }: Props) {
     onDelete(checklistItemId)
   }
 
+  const getText = () => {
+    if (routine.type === SCHEDULE_TYPES.monthly) return 'next month'
+    if (routine.type === SCHEDULE_TYPES.weekly) return 'next week'
+    return 'tomorrow'
+  }
+
   return (
     <div className="w-full py-4">
-      <h4 className="text-sm font-bold uppercase text-gray-400">Checklist</h4>
-      <p className="text-xs text-gray-300">Checklist will be automaticaly reset next month</p>
+      <div className="flex flex-wrap items-start justify-between gap-x-8">
+        <h4 className="font-bold uppercase text-gray-400">Checklist</h4>
+        <p className="mt-1 text-xs text-gray-300">checklist will automatically reset {getText()}</p>
+      </div>
 
       <div className="mt-4 flex flex-col gap-2">
         {routine.checklist?.map((checklistItem) => (
