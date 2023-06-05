5b144a225d7ce2a68e5979e4f8c683542704b38b
mimccio <miklmccio@gmail.com>
2023-06-05 15:12:20 +0200

    add isMutating feedback when online

--- src/common/components/fetching-spinner.tsx
+++ src/common/components/fetching-spinner.tsx
@@ -1,15 +1,18 @@
-import { useIsFetching } from '@tanstack/react-query'
+import { useIsFetching, useIsMutating, onlineManager } from '@tanstack/react-query'
 import { Transition } from '@headlessui/react'
 import { DotLoader } from 'react-spinners'
 import { SPINNER_COLOR } from '../constants'
 
 export function FetchingSpinner() {
   const fetchingNum = useIsFetching()
+  const mutatingNum = useIsMutating()
+  const isOnline = onlineManager.isOnline()
+  const isMutating = isOnline && mutatingNum >= 1
 
   return (
     <Transition
       appear
-      show={fetchingNum >= 1}
+      show={fetchingNum >= 1 || isMutating}
       className="fixed bottom-4 right-4 z-30 md:bottom-6 md:right-6"
       enter="ease-out duration-200"
       enterFrom="opacity-0"
