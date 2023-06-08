4738f2d54e1e43a035a04dd45db1dbd72928e488
mimccio <miklmccio@gmail.com>
2023-06-08 21:48:20 +0200

    folder list layout

--- src/screens/note-list/parts/folder-list.tsx
+++ src/screens/note-list/parts/folder-list.tsx
@@ -9,7 +9,7 @@ export function FolderList() {
   const { folderList, isLoading } = useFolderList()
 
   return (
-    <div className="flex flex-col p-4">
+    <div className="flex flex-col px-4">
       <NewFolderItem />
       <Link to="/notes/inbox" className="flex items-center gap-x-4 p-2">
         <InboxIcon width={20} className="text-gray-500" /> <span className="font-semibold text-gray-600">Inbox</span>
