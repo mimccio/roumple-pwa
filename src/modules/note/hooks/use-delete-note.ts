d1657528d24f49f00588473325045a488843ea80
mimccio <miklmccio@gmail.com>
2023-06-15 01:59:25 +0200

    handle folder count when delete note

--- src/modules/note/hooks/use-delete-note.ts
+++ src/modules/note/hooks/use-delete-note.ts
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
 
 import { useMainPath } from '&/common/hooks'
+import type { NoteFolder } from '&/modules/note-folder/types'
+import { NOTE_FOLDER_KEYS } from '&/modules/note-folder/constants'
 import type { Note } from '../types'
 import { NOTE_KEYS } from '../constants'
 import { deleteNote } from '../mutations'
-
-// TODO!: handle count in folder
 
 export function useDeleteNote() {
   const queryClient = useQueryClient()
@@ -25,18 +25,50 @@ export function useDeleteNote() {
         const noteIndex = old.findIndex((item) => item.id === data.id)
         return [...old.slice(0, noteIndex), ...old.slice(noteIndex + 1)]
       })
+
+      const previousFolderListAllCategory = queryClient.getQueryData(NOTE_FOLDER_KEYS.list({ categoryId: undefined }))
+      queryClient.setQueryData(NOTE_FOLDER_KEYS.list({ categoryId: undefined }), (old: NoteFolder[] = []) => {
+        const folderIndex = old.findIndex((item) => item.id === data.folder?.id)
+        if (folderIndex < 0) return old
+        const oldFolder = old[folderIndex]
+        const count = (oldFolder.noteCount?.[0].count || 1) - 1
+        const newFolder = { ...oldFolder, noteCount: [{ count }] }
+        return [...old.slice(0, folderIndex), newFolder, ...old.slice(folderIndex + 1)]
+      })
+
+      const previousFolderListCategoryId = queryClient.getQueryData(
+        NOTE_FOLDER_KEYS.list({ categoryId: data.category?.id })
+      )
+      queryClient.setQueryData(NOTE_FOLDER_KEYS.list({ categoryId: data.category?.id }), (old: NoteFolder[] = []) => {
+        if (!data.category?.id) return old
+        const folderIndex = old.findIndex((item) => item.id === data.folder?.id)
+        if (folderIndex < 0) return old
+        const oldFolder = old[folderIndex]
+        const count = (oldFolder.noteCount?.[0].count || 1) - 1
+        const newFolder = { ...oldFolder, noteCount: [{ count }] }
+        return [...old.slice(0, folderIndex), newFolder, ...old.slice(folderIndex + 1)]
+      })
+
       navigate(mainPath)
-      return { previousNoteList }
+      return { previousNoteList, previousFolderListAllCategory, previousFolderListCategoryId }
     },
 
     onError: (_err, item, context) => {
       queryClient.setQueryData(NOTE_KEYS.detail(item.id), item)
       queryClient.setQueryData(NOTE_KEYS.list({ folderId: item.folder?.id }), context?.previousNoteList)
+      queryClient.setQueryData(NOTE_FOLDER_KEYS.list({ categoryId: undefined }), context?.previousFolderListAllCategory)
+      queryClient.setQueryData(
+        NOTE_FOLDER_KEYS.list({ categoryId: item.category?.id }),
+        context?.previousFolderListCategoryId
+      )
+
       toast.error("Deletion didn't work")
     },
     onSuccess: (_data, variables) => {
       queryClient.invalidateQueries(NOTE_KEYS.detail(variables.id))
       queryClient.invalidateQueries(NOTE_KEYS.list({ folderId: variables.folder?.id }))
+      queryClient.invalidateQueries(NOTE_FOLDER_KEYS.list({ categoryId: undefined }))
+      queryClient.invalidateQueries(NOTE_FOLDER_KEYS.list({ categoryId: variables.category?.id }))
     },
   })
 
