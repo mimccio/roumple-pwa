3a986f6ed7a36535a02f6a30e891c47574629b9c
mimccio <miklmccio@gmail.com>
2023-06-15 00:40:05 +0200

    border

--- src/common/components/menus/editor-menu/editor-menu.tsx
+++ src/common/components/menus/editor-menu/editor-menu.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 export function EditorMenu({ editor }: Props) {
   return (
-    <div className="flex justify-center gap-1 border-t border-gray-100 bg-gray-50 py-1">
+    <div className="flex justify-center gap-1 border-b border-gray-100 bg-gray-50 py-1">
       <input id="focusdummy" className="opacity:0 w-0" />
       <EditorMenuButton
         Icon={H1Icon}
