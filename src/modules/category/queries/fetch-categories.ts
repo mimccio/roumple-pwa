5b1916cdb59bcf88aa232c53c22004ef31b00634
mimccio <miklmccio@gmail.com>
2023-06-14 04:24:05 +0200

    add limit 100 to fetch categories

--- src/modules/category/queries/fetch-categories.ts
+++ src/modules/category/queries/fetch-categories.ts
@@ -1,7 +1,7 @@
 import { db } from '&/db'
 
 export const fetchCategories = async () => {
-  const { data, error } = await db.from('category').select('id, name, color').order('created_at')
+  const { data, error } = await db.from('category').select('id, name, color').order('created_at').limit(100)
 
   if (error) throw error
   return data
