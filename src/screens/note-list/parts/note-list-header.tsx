0e19f41d2e577bc4b95e4607c75faf994c5f1161
mimccio <miklmccio@gmail.com>
2023-06-17 00:48:55 +0200

    plus icon for new note

--- src/screens/note-list/parts/note-list-header.tsx
+++ src/screens/note-list/parts/note-list-header.tsx
@@ -1,4 +1,4 @@
-import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { DocumentTextIcon } from '@heroicons/react/24/outline'
 
 import { Tooltip } from '&/common/components/tooltip'
@@ -12,15 +12,14 @@ export function NoteListHeader() {
 
   return (
     <Header>
-      <div className=" text flex h-full items-center text-xl font-bold leading-6 text-gray-500">
-        <DocumentTextIcon width={20} className="text-gray-500" />
-
-        <h1 className="ml-2">Notes</h1>
+      <div className="flex h-full items-center text-xl font-bold leading-6">
+        <DocumentTextIcon width={20} className="text-gray-400" />
+        <h1 className="ml-2 text-gray-500">Notes</h1>
       </div>
-      <div className="flex gap-1">
+      <div className="flex gap-2">
         <Tooltip message="create note">
-          <button className="group p-2" onClick={onCreate}>
-            <PlusCircleIcon width={20} className="text-gray-400 transition-colors group-hover:text-gray-500" />
+          <button className="group rounded-md p-1" onClick={onCreate}>
+            <PlusIcon width={24} className="text-gray-500 transition-colors group-hover:text-gray-600" />
           </button>
         </Tooltip>
 
