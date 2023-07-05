bc7b09bb34e20414105b37836182d44ec508ec34
mimccio <miklmccio@gmail.com>
2023-07-05 22:31:03 +0200

    clear checked_item_ids on reset (db)

--- src/modules/task/mutations/edit-task-status.ts
+++ src/modules/task/mutations/edit-task-status.ts
@@ -1,7 +1,7 @@
 import { db } from '&/db'
 import { Task } from '../types'
 
-export const editTaskStatus = async ({ id, status }: Task) => {
-  const { error } = await db.from('task').update({ status }).eq('id', id)
+export const editTaskStatus = async ({ id, status, checkedItemIds }: Task) => {
+  const { error } = await db.from('task').update({ status, checked_item_ids: checkedItemIds }).eq('id', id)
   if (error) throw error
 }
