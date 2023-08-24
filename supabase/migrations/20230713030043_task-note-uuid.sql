f56341561ad84bcd51105e2267339350d11d77ad
mimccio <miklmccio@gmail.com>
2023-08-24 07:47:01 +0200

    remove old migration files

--- supabase/migrations/20230713030043_task-note-uuid.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."task_note" alter column "id" set data type uuid using "id"::uuid;
-
-CREATE UNIQUE INDEX routine_note_pkey ON public.routine_note USING btree (id);
-
-alter table "public"."routine_note" add constraint "routine_note_pkey" PRIMARY KEY using index "routine_note_pkey";
-
-
