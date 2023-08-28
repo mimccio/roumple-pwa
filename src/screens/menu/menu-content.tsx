8648c4fca9734e3b9245d8016f47086e1b5bf000
mimccio <miklmccio@gmail.com>
2023-08-28 12:15:37 +0200

    menu full height

--- src/screens/menu/menu-content.tsx
+++ src/screens/menu/menu-content.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function MenuContent({ close }: Props) {
   return (
-    <div className="no-scrollbar flex h-screen min-h-screen flex-col overflow-y-auto bg-gray-50 px-4">
+    <div className="no-scrollbar flex h-full min-h-screen flex-col overflow-y-auto bg-gray-50 px-4">
       <BoardNav close={close} />
       <MainNav close={close} />
       <Categories />
