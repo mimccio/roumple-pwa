ef06e76ca605f4dc4e83f3ac50f76530bf94ff46
mimccio <miklmccio@gmail.com>
2023-09-01 11:48:45 +0200

    refactor empty tasks

--- src/screens/task-list/task-list-screen.tsx
+++ src/screens/task-list/task-list-screen.tsx
@@ -1,20 +1,14 @@
-import { Trans, useTranslation } from 'react-i18next'
-
 import { ContentLayout, MainListLayout } from '&/common/components/layouts'
 import { ListSkeleton } from '&/common/components/list-skeleton'
-import { EmptyMainContent } from '&/common/components/empty-screens'
-import workflowImg from '&/assets/illustrations/workflow.png'
-import successImg from '&/assets/illustrations/success.png'
-import locationImg from '&/assets/illustrations/location.png'
 
 import { useTaskList } from '&/modules/task/hooks'
 import { CreateTaskModale } from '&/modules/task/components'
 import { MainError, OfflineError } from '../errors'
 import { TaskListHeader } from './parts/task-list-header'
 import { TaskListContent } from './parts/task-list-content'
+import { EmptyContent } from './parts/empty-content'
 
 export function TaskListScreen() {
-  const { t } = useTranslation('task')
   const {
     createIsOpen,
     handleDoneChange,
@@ -45,20 +39,7 @@ export function TaskListScreen() {
           {showStatus.data && !showDone && <TaskListContent list={taskList} />}
         </MainListLayout>
 
-        {showStatus.empty && !showDone && (
-          <EmptyMainContent onClick={onOpenCreate} text={t('createNewTask')} image={workflowImg} />
-        )}
-        {showStatus.empty && showDone && <EmptyMainContent text={t('noDoneTask')} image={successImg} />}
-        {showStatus.emptyFilteredList && category && (
-          <EmptyMainContent
-            text={
-              <Trans t={t} i18nKey="noTaskWithCategory" values={{ category: category.name }}>
-                No task with <span className="font-semibold">{category.name}</span> category
-              </Trans>
-            }
-            image={locationImg}
-          />
-        )}
+        <EmptyContent showStatus={showStatus} showDone={showDone} onOpenCreate={onOpenCreate} category={category} />
       </ContentLayout>
       <CreateTaskModale isOpen={createIsOpen} close={onCloseCreate} />
     </>
