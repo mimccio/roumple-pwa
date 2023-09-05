f7333d4f41365527f733bf49cdf3acc11aa18b45
mimccio <miklmccio@gmail.com>
2023-09-05 14:50:19 +0200

    menu bg

--- src/common/components/menus/item-menu/item-menu.tsx
+++ src/common/components/menus/item-menu/item-menu.tsx
@@ -52,7 +52,7 @@ export function ItemMenu({
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-gray-50 shadow-sm ring-1 ring-gray-400 ring-opacity-20 focus:outline-none">
           <div className="py-1">
             {withCopyLink && <CopyUrlToClipboard />}
             {onLinkNote && (
--- src/screens/task-list/parts/tasks-menu/tasks-menu.tsx
+++ src/screens/task-list/parts/tasks-menu/tasks-menu.tsx
@@ -37,7 +37,7 @@ export function TasksMenu({ handleSortChange }: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 w-56 origin-top-left rounded-md bg-gray-50 shadow-sm ring-1 ring-gray-400 ring-opacity-20 focus:outline-none">
           <div className="py-1 ">
             <MenuBtn Icon={AdjustmentsVerticalIcon} handleClick={() => handleSortChange(SORT_TYPES.date)}>
               {t('sortByDate', { ns: 'action' })}
