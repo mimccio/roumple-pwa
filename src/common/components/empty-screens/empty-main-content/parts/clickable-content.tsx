dab916615bdd992a39d8f4943e1abba164e41479
mimccio <miklmccio@gmail.com>
2023-09-13 01:53:04 +0200

    board page accessibility

--- src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
@@ -12,6 +12,7 @@ export function ClickableContent({ image, text, onClick }: Props) {
       <img
         src={image}
         className="mx-auto flex h-52 w-52 items-center justify-center opacity-25 transition-opacity duration-500 group-hover:opacity-50"
+        aria-hidden="true"
       />
       <p className="text-center text-sm font-semibold text-gray-300 transition-colors duration-500 group-hover:text-gray-400">
         {text}
--- src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
@@ -8,7 +8,7 @@ interface Props {
 export function SimpleContent({ image, text }: Props) {
   return (
     <div className="mb-20 flex w-full flex-col items-center justify-center gap-4 py-8">
-      <img src={image} className="mx-auto flex h-52 w-52 items-center justify-center opacity-25" />
+      <img src={image} className="mx-auto flex h-52 w-52 items-center justify-center opacity-25" aria-hidden="true" />
       <p className="text-center text-sm font-semibold text-gray-300">{text}</p>
     </div>
   )
