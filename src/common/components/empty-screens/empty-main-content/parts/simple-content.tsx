bb39a84b126a063d437b1eff39be8556e0d36260
mimccio <miklmccio@gmail.com>
2023-09-13 01:55:42 +0200

    empty alt

--- src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
@@ -8,7 +8,12 @@ interface Props {
 export function SimpleContent({ image, text }: Props) {
   return (
     <div className="mb-20 flex w-full flex-col items-center justify-center gap-4 py-8">
-      <img src={image} className="mx-auto flex h-52 w-52 items-center justify-center opacity-25" aria-hidden="true" />
+      <img
+        src={image}
+        className="mx-auto flex h-52 w-52 items-center justify-center opacity-25"
+        aria-hidden="true"
+        alt=""
+      />
       <p className="text-center text-sm font-semibold text-gray-300">{text}</p>
     </div>
   )
