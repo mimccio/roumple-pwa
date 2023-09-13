8593ade8b95e5c844736974165e5adc45280ecb0
mimccio <miklmccio@gmail.com>
2023-09-13 02:10:55 +0200

    undo accessibility

--- src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
@@ -15,7 +15,7 @@ export function ClickableContent({ image, text, onClick }: Props) {
         className="mx-auto flex h-52 w-52 items-center justify-center opacity-25 transition-opacity duration-500 group-hover:opacity-50"
         aria-hidden="true"
       />
-      <p className="text-center text-sm font-semibold text-gray-400 transition-colors duration-500 group-hover:text-gray-500">
+      <p className="text-center text-sm font-semibold text-gray-300 transition-colors duration-500 group-hover:text-gray-400">
         {text}
       </p>
     </button>
