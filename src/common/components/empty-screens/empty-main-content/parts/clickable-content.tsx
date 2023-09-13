ba3f5f9f8635da1842d77ad8c99e0e8ec6303e78
mimccio <miklmccio@gmail.com>
2023-09-13 02:03:15 +0200

    empty alt 2

--- src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/clickable-content.tsx
@@ -10,10 +10,10 @@ export function ClickableContent({ image, text, onClick }: Props) {
   return (
     <button onClick={onClick} className="group mb-20 flex w-full flex-col items-center justify-center gap-4 py-8">
       <img
+        alt=""
         src={image}
         className="mx-auto flex h-52 w-52 items-center justify-center opacity-25 transition-opacity duration-500 group-hover:opacity-50"
         aria-hidden="true"
-        alt=""
       />
       <p className="text-center text-sm font-semibold text-gray-300 transition-colors duration-500 group-hover:text-gray-400">
         {text}
--- src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
+++ src/common/components/empty-screens/empty-main-content/parts/simple-content.tsx
@@ -9,10 +9,10 @@ export function SimpleContent({ image, text }: Props) {
   return (
     <div className="mb-20 flex w-full flex-col items-center justify-center gap-4 py-8">
       <img
+        alt=""
         src={image}
         className="mx-auto flex h-52 w-52 items-center justify-center opacity-25"
         aria-hidden="true"
-        alt=""
       />
       <p className="text-center text-sm font-semibold text-gray-300">{text}</p>
     </div>
