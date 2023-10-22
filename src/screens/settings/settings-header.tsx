ebd8a4e93236ba7e59725c7d046c6ecf6501c7c3
mimccio <miklmccio@gmail.com>
2023-10-23 01:18:22 +0200

    new settings layout

--- src/screens/settings/settings-header.tsx
+++ src/screens/settings/settings-header.tsx
@@ -4,7 +4,7 @@ import { Cog6ToothIcon } from '@heroicons/react/20/solid'
 export function SettingsHeader() {
   const { t } = useTranslation('common')
   return (
-    <header className="flex h-14 w-full items-center justify-between border-b-4 border-gray-200 px-2">
+    <header className="flex h-14 w-full items-center justify-between border-b-4 border-gray-200 px-4">
       <div className="text flex h-full items-center text-xl font-bold leading-6 text-gray-500">
         <Cog6ToothIcon width={20} className="text-gray-500" />
 
--- src/screens/settings/settings-main.tsx
+++ src/screens/settings/settings-main.tsx
@@ -1,25 +1,33 @@
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import { DocumentDuplicateIcon, UserIcon } from '@heroicons/react/24/outline'
+
 import { SettingsHeader } from './settings-header'
-import { DocumentDuplicateIcon } from '@heroicons/react/24/outline'
+import { Section } from './parts/section'
 
 export function SettingsMain() {
   const { t } = useTranslation(['login', 'settings'])
   return (
     <>
       <SettingsHeader />
-      <div className="mt-8 w-full px-4">
-        <div className="flex justify-end">
-          <Link to="/logout" className="rounded-md border p-2">
+      <div className="mt-4 flex w-full flex-col gap-y-8 p-4">
+        <Section title="Templates" Icon={DocumentDuplicateIcon}>
+          <Link
+            to="/templates"
+            className="group flex items-center gap-x-2 font-semibold text-indigo-500 transition-colors hover:text-indigo-600"
+          >
+            {t('View templates', { ns: 'settings' })}
+          </Link>
+        </Section>
+
+        <Section title="Account" Icon={UserIcon}>
+          <Link
+            to="/logout"
+            className="rounded-md bg-gray-100 px-4 py-2 font-semibold text-gray-700 transition-colors hover:bg-gray-200 hover:text-gray-800"
+          >
             {t('logout', { ns: 'login' })}
           </Link>
-        </div>
-        <div className="mt-8">
-          <Link to="/templates" className="group flex items-center gap-x-2">
-            <DocumentDuplicateIcon className="w-5 text-gray-500 group-hover:text-indigo-400" />
-            <span className="group-hover:text-indigo-500">{t('View templates', { ns: 'settings' })}</span>
-          </Link>
-        </div>
+        </Section>
       </div>
     </>
   )
