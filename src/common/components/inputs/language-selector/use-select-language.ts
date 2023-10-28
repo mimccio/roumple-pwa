344de9f9b1070995ae8bf7df86f741ef3c7488da
mimccio <miklmccio@gmail.com>
2023-10-28 02:49:38 +0200

    handle lang in html tag

--- src/common/components/inputs/language-selector/use-select-language.ts
+++ src/common/components/inputs/language-selector/use-select-language.ts
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import type { Language } from '&/common/types'
 import { LANGUAGES } from '&/common/constants'
 import { useGetLanguage } from '&/common/hooks'
+import toast from 'react-hot-toast'
 
 const LANGUAGE_OPTIONS = [
   { value: LANGUAGES.en, text: 'English' },
@@ -12,7 +13,15 @@ const LANGUAGE_OPTIONS = [
 export function useSelectLanguage() {
   const { i18n } = useTranslation()
   const language = useGetLanguage()
-  const handleSelectLanguage = (newLang: Language) => i18n.changeLanguage(newLang)
+  const handleSelectLanguage = (newLang: Language) => {
+    i18n.changeLanguage(newLang, (error, t) => {
+      if (error) {
+        toast.error(t('editLangError', { ns: 'error' }))
+      } else {
+        document.documentElement.lang = newLang
+      }
+    })
+  }
 
   return {
     handleSelectLanguage,
