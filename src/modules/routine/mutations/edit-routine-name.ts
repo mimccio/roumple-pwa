491ddf0bdd486a8b14dcf3c67ec53d1eb772c844
mimccio <miklmccio@gmail.com>
2023-11-04 13:03:15 +0100

    remove fake error

--- src/modules/routine/mutations/edit-routine-name.ts
+++ src/modules/routine/mutations/edit-routine-name.ts
@@ -3,7 +3,5 @@ import { Routine } from '../types'
 
 export const editRoutineName = async ({ id, name }: Pick<Routine, 'id' | 'name'>) => {
   const { error } = await db.from('routine').update({ name }).eq('id', id)
-  throw new Error('name')
-
   if (error) throw error
 }
