01eabe2bc8ab55955e5c231b469e6bffc79e7bd3
mimccio <miklmccio@gmail.com>
2023-11-04 13:04:12 +0100

    clean

--- src/modules/routine/mutations/edit-routine-priority.ts
+++ src/modules/routine/mutations/edit-routine-priority.ts
@@ -2,8 +2,6 @@ import { db } from '&/db'
 import { Routine } from '../types'
 
 export const editRoutinePriority = async ({ id, priority }: Routine) => {
-  // throw new Error('nb')
-
   const { data, error } = await db.from('routine').update({ priority }).eq('id', id).select('*').single()
   if (error) throw error
   return data
