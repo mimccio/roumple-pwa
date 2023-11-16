29095b013e92481664adabfb0b23a06b3c8e3135
mimccio <miklmccio@gmail.com>
2023-11-16 19:46:51 +0100

    note with new layout

--- src/screens/note-details/note-details-screen.tsx
+++ src/screens/note-details/note-details-screen.tsx
@@ -1,7 +1,11 @@
 import { DetailsLoadingPage } from '&/common/components/details-loading-page'
+import { CreatedAt } from '&/common/components/display/created-at'
+
 import { useGetNoteDetail } from '&/modules/note/hooks'
 import { NotFoundDetails, OfflineError } from '../errors'
+
 import { InfoSection } from './parts/info-section'
+import { NoteCategory } from './parts/note-category'
 import { NoteEditor } from './parts/note-editor'
 import { NoteNavbar } from './parts/note-navbar'
 
@@ -15,8 +19,10 @@ export function NoteDetailsScreen() {
   return (
     <>
       <NoteNavbar note={note} />
+      <NoteCategory note={note} />
       <InfoSection note={note} routineNoteList={routineNoteList} />
       <NoteEditor note={note} />
+      <CreatedAt createdAt={note.created_at} />
     </>
   )
 }
--- src/screens/note-details/parts/info-section.tsx
+++ src/screens/note-details/parts/info-section.tsx
@@ -1,7 +1,5 @@
-import { CreatedAt } from '&/common/components/display/created-at'
 import { Note } from '&/modules/note/types'
 import { RoutineNoteByNote } from '&/modules/routine-note/types'
-import { NoteCategory } from './note-category'
 import { NoteFolder } from './note-folder'
 import { NoteLinks } from './note-links'
 
@@ -13,10 +11,8 @@ interface Props {
 export function InfoSection({ note, routineNoteList }: Props) {
   return (
     <section className="flex flex-col gap-y-1 border-b bg-gray-100 px-4 py-2">
-      <NoteCategory note={note} />
       <NoteFolder note={note} />
       <NoteLinks note={note} routineNoteList={routineNoteList} />
-      <CreatedAt createdAt={note.created_at} />
     </section>
   )
 }
