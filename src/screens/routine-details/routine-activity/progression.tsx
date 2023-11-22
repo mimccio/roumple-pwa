2625a448b2e5b9faeef773d160ad5f1051661f9d
mimccio <miklmccio@gmail.com>
2023-11-22 01:08:56 +0100

    smaller epsillon

--- src/screens/routine-details/routine-activity/progression.tsx
+++ src/screens/routine-details/routine-activity/progression.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function Progression({ prev, cur, isLast }: Props) {
   if (prev == undefined) return null
-  if (Math.abs(cur - prev) < 5) return <Bars2Icon className="w-6 text-blue-500" />
+  if (Math.abs(cur - prev) < 4) return <Bars2Icon className="w-6 text-blue-500" />
   if (cur > prev) return <ArrowTrendingUpIcon className="w-6 text-green-500" />
   if (!isLast && cur < prev) return <ArrowTrendingDownIcon className="w-6 text-orange-500" />
   return null
