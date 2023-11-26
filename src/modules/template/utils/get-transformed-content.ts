c3cb9dce93dbdcb00bce009e044e83fe496923a5
mimccio <miklmccio@gmail.com>
2023-11-26 23:11:48 +0100

    clean

--- src/modules/template/utils/get-transformed-content.ts
+++ src/modules/template/utils/get-transformed-content.ts
@@ -4,10 +4,10 @@ import { v5 as uuidv5 } from 'uuid'
 
 const getTransformPath = (userId: string, path: string) => {
   const pathArr = path.split('/')
-  console.log('pathArr :', pathArr)
+
   if (pathArr[1] !== 'templates') return path
   const listType = pathArr[3]
-  console.log('listType :', listType)
+
   let newPath = path
   if (listType === 'routines') {
     newPath = pathArr[4] ? `/routines/d/routine/${uuidv5(pathArr[4], userId)}/` : '/routines'
@@ -23,7 +23,7 @@ const getTransformPath = (userId: string, path: string) => {
   } else if (listType === 'categories') {
     newPath = '/categories'
   }
-  console.log('newPath :', newPath)
+
   return newPath
 }
 
@@ -36,15 +36,15 @@ const getOrigin = (hostname: string) => {
 
 const getTransformedUrl = (userId: string, href: string) => {
   const url = new URL(href)
-  console.log('url :', url)
+
   const isRoumple =
     url.hostname === 'admin.roumple.com' || url.hostname === 'dev.admin.roumple.com' || url.hostname === 'localhost'
-  console.log('isRoumple :', isRoumple)
+
   if (!isRoumple) return href
 
   const pathname = getTransformPath(userId, url.pathname)
   const transformedHref = new URL(pathname, getOrigin(url.hostname)).href
-  console.log('transformedHref :', transformedHref)
+
   return transformedHref
 }
 
