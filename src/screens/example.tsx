115974e61f2dc9d66b6c111765a3d70d38af2de4
mimccio <miklmccio@gmail.com>
2023-12-07 13:51:25 +0100

    init shadcn/ui with css variables

--- src/screens/example.tsx
+++ src/screens/example.tsx
@@ -3,7 +3,9 @@ import { Button } from '@/common/ui/button'
 export function Example() {
   return (
     <div className="p-4">
-      <Button>hello</Button>
+      <Button variant="link" size="sm">
+        hello
+      </Button>
     </div>
   )
 }
